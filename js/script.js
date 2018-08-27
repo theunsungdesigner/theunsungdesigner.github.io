7d41c348e469fe8a1ab6677d2e3f433d28e3b26b
theunsungdesigner <theunsungdesigner@gmail.com>
2018-08-27 18:55:10 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$("document").ready(function(){
+	$('.imageGallery1').masonry({
+  		// options
+  		itemSelector: '.imgItem',
+  		columnWidth: 300,
+  		columnHeight: 300,
+  		
+	
+	});
+
+
+});
